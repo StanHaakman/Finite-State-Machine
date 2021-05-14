5353663ad8f86f753ecb63b8816150b2cb51031f
Stan Haakman <stanhaakman@icloud.com>
2021-05-14 14:18:06 +0200

    Unit test voor de Node afmaken

--- test/fsm/domain/NodeTest.java
+++ test/fsm/domain/NodeTest.java
@@ -2,16 +2,35 @@ package fsm.domain;
 
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class NodeTest {
 
     @Test
     public void addTriggerPoint() {
+        Node s0 = new Node("Test trigger points");
+        s0.addTriggerPoint('A', s0);
+
+//        Create a map with the desired result
+        Map<Character, Node> desiredResult = new HashMap<>();
+        desiredResult.put('A', s0);
+
+        assertEquals(s0.getTriggerPoints(), desiredResult);
     }
 
     @Test
     public void getTriggerPoints() {
+        Node s0 = new Node("Test trigger points");
+        s0.addTriggerPoint('A', s0);
+
+//        Create a map with the desired result
+        Map<Character, Node> desiredResult = new HashMap<>();
+        desiredResult.put('A', s0);
+
+        assertEquals(s0.getTriggerPoints(), desiredResult);
     }
 
     @Test
